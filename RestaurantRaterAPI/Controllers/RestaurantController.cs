eac2992efa7f10a70379383fa1dda5a92de5fa62
sgsettle <s.settle41@gmail.com>
2020-08-25 15:10:48 -0400

    Built out GET methods

--- RestaurantRaterAPI/Controllers/RestaurantController.cs
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRaterAPI.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -11,11 +12,15 @@ namespace RestaurantRaterAPI.Controllers
 {
     public class RestaurantController : ApiController
     {
-        RestaurantDBContext _context = new RestaurantDBContext();
+        private readonly RestaurantDBContext _context = new RestaurantDBContext();
         
         // CREATE (POST)
         public async Task<IHttpActionResult> PostRestaurant(Restaurant model)
         {
+            if (model == null)
+            {
+                return BadRequest("Your request body cannot be empty.");
+            }
             // check to see if valid model based on criteria in restaurant class
             if (ModelState.IsValid)
             {
@@ -25,7 +30,7 @@ namespace RestaurantRaterAPI.Controllers
                 await _context.SaveChangesAsync();
 
                 // return 200 response
-                return Ok();
+                return Ok("You created a restaurant and it was saved!");
             }
 
             // if model is not valid, return bad request
@@ -34,8 +39,26 @@ namespace RestaurantRaterAPI.Controllers
 
         // READ (GET)
         // Get by ID
+        [HttpGet]
+        public async Task<IHttpActionResult> GetById(int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            if (restaurant != null)
+            {
+                return Ok(restaurant);
+            }
+
+            return NotFound();
+        }
 
         // Get All
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
+            return Ok(restaurants);
+        }
 
         // UPDATE (PUT)
 
