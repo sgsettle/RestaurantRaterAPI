f2e31d7ef30b409ebc6ab073b16de27677e67bf2
sgsettle <s.settle41@gmail.com>
2020-08-27 10:24:15 -0400

    finished api

--- RestaurantRaterAPI/Controllers/RatingController.cs
+++ RestaurantRaterAPI/Controllers/RatingController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRaterAPI.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -42,11 +43,78 @@ namespace RestaurantRaterAPI.Controllers
         }
 
         // Get a rating by its ID?
+        [Route("api/Rating/RatingById")]
+        public async Task<IHttpActionResult> GetById(int id)
+        {
+            Rating rating = await _context.Ratings.FindAsync(id);
+
+            if (rating != null)
+            {
+                return Ok(rating);
+            }
+
+            return NotFound();
+        }
 
         // Get ALL ratings for a specific restaurant by the restaurant ID
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAllRatingsByRestaurantId(int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+            //List<Rating> ratings = await _context.Ratings.ToListAsync();
+
+            if (restaurant != null)
+            {
+                return Ok(restaurant);
+            }
+
+            return NotFound();
+        }
 
         // Update Rating
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateRating([FromUri] int id, [FromBody] Rating updatedRating)
+        {
+            if (ModelState.IsValid)
+            {
+                Rating rating = await _context.Ratings.FindAsync(id);
+
+                if (rating != null)
+                {
+                    rating.FoodScore = updatedRating.FoodScore;
+                    rating.EnvironmentScore = updatedRating.EnvironmentScore;
+                    rating.CleanlinessScore = updatedRating.CleanlinessScore;
+
+                    await _context.SaveChangesAsync();
+
+                    return Ok("Rating has been updated.");
+                }
+
+                return NotFound();
+            }
+
+            return BadRequest(ModelState);
+        }
 
         // Delete rating
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteRatingById(int id)
+        {
+            Rating entity = await _context.Ratings.FindAsync(id);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            _context.Ratings.Remove(entity);
+
+            if (await _context.SaveChangesAsync() == 1)
+            {
+                return Ok("The rating was deleted.");
+            }
+
+            return InternalServerError();
+        }
     }
 }
